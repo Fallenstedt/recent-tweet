4b4239cc670156edcf80718a04f4f151e5e9c055
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-11-01 20:35:10 -0700

    setup twitter client

---
+++ get-tweet/lib/credentials.go
@@ -0,0 +1,53 @@
+package lib
+
+import (
+	"log"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+// other imports
+// "github.com/dghubble/oauth1"
+
+// Credentials stores all of our access/consumer tokens
+// and secret keys needed for authentication against
+// the twitter REST API.
+type Credentials struct {
+	ConsumerKey       string
+	ConsumerSecret    string
+	AccessToken       string
+	AccessTokenSecret string
+}
+
+// GetClient returns a Twitter Client
+// Used to fetch information
+// About your twitter self
+func (c *Credentials) GetClient() (*twitter.Client, error) {
+	config, token := getConfigToken(c)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	// Verify Credentials
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+
+	// we can retrieve the user and verify if the credentials
+	// we have used successfully allow us to log in!
+	user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("User's ACCOUNT:\n%+v\n", user)
+	return client, nil
+}
+
+func getConfigToken(c *Credentials) (*oauth1.Config, *oauth1.Token) {
+	config := oauth1.NewConfig(c.ConsumerKey, c.ConsumerSecret)
+	token := oauth1.NewToken(c.AccessToken, c.AccessTokenSecret)
+	return config, token
+}
