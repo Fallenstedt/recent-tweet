5037eb31e0f0246e6e1d79f73277881d412d75d7
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-11-02 21:47:12 -0700

    fetch recent tweet

--- get-tweet/main.go
+++ get-tweet/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/fallenstedt/latest-tweet/get-tweet/lib"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	twitterFactory := lib.Credentials{
+		ConsumerKey:       os.Getenv("CONSUMER_KEY"),
+		ConsumerSecret:    os.Getenv("CONSUMER_SECRET"),
+		AccessToken:       os.Getenv("ACCESS_KEY"),
+		AccessTokenSecret: os.Getenv("ACCESS_SECRET"),
+	}
+
+	client, err := twitterFactory.GetClient()
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	tweets, _, err := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
+		Count: 1,
+	})
+
+	tweetsJSON, err := json.Marshal(tweets)
+
+	if err == nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       string("hello"),
+		Body:       string(tweetsJSON),
 		StatusCode: 200,
 	}, nil
 }
