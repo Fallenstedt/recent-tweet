9216f787265f014ee543164b6df104a67734e856
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-11-03 18:25:21 -0800

    create table skeleton

---
+++ get-tweet/lib/simple-tweet-table.go
@@ -0,0 +1,21 @@
+package lib
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func CreateSimpleTweetTableSession() (*dynamodb.DynamoDB, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	svc := dynamodb.New(sess)
+
+	return svc, nil
+}
