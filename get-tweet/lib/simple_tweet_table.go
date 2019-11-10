3e95b4f6be885e1d637ddbea0b68075c1be5b838
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-11-09 22:06:36 -0800

    implement strategy for working with latest tweet from dynamo

--- get-tweet/lib/simple_tweet_table.go
+++ get-tweet/lib/simple_tweet_table.go
@@ -10,15 +10,15 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-// SimpleTweetTable returns a DynamoDB session
+// DynamoDbInstance returns a DynamoDB session
 // to the table it talks to.
-type SimpleTweetTable struct {
-	session   *dynamodb.DynamoDB
+type DynamoDbInstance struct {
+	Session   *dynamodb.DynamoDB
 	TableName string
 }
 
 // CreateSimpleTweetTableSession returns a dynamodb session
-func CreateSimpleTweetTableSession(tableName string) *SimpleTweetTable {
+func CreateSimpleTweetTableSession(tableName string) *DynamoDbInstance {
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -26,16 +26,35 @@ func CreateSimpleTweetTableSession(tableName string) *SimpleTweetTable {
 
 	svc := dynamodb.New(sess)
 
-	obj := SimpleTweetTable{
-		session:   svc,
+	obj := DynamoDbInstance{
+		Session:   svc,
 		TableName: tableName,
 	}
 	return &obj
 }
 
-//QueryTweetFromDynamo gets the tweet from Dynamo if it exists
-func (s *SimpleTweetTable) QueryTweetFromDynamo(t *SimpleTweetDTO) *SimpleTweetDTO {
-	result, err := s.session.GetItem(&dynamodb.GetItemInput{
+// DynamoOperator defines a single operation that
+// can be operated on our database
+type DynamoOperator interface {
+	Execute(*DynamoDbInstance, *SimpleTweetDTO) *SimpleTweetDTO
+}
+
+// DynamoOperation defines a single Operator that can be executed
+type DynamoOperation struct {
+	DynamoOperator DynamoOperator
+}
+
+// ExecuteOperation allows us to call the chosen DynamoOperator's `Execute` function
+func (o *DynamoOperation) ExecuteOperation(s *DynamoDbInstance, t *SimpleTweetDTO) *SimpleTweetDTO {
+	return o.DynamoOperator.Execute(s, t)
+}
+
+// QueryTweet is a DynamoOperator that allows us to Query a Tweet
+type QueryTweet struct{}
+
+// Execute from QueryTweet allows us to fetch the latest tweet from DynamoDB
+func (QueryTweet) Execute(s *DynamoDbInstance, t *SimpleTweetDTO) *SimpleTweetDTO {
+	result, err := s.Session.GetItem(&dynamodb.GetItemInput{
 		TableName: aws.String(s.TableName),
 		Key: map[string]*dynamodb.AttributeValue{
 			"ID": {
@@ -58,7 +77,11 @@ func (s *SimpleTweetTable) QueryTweetFromDynamo(t *SimpleTweetDTO) *SimpleTweetD
 	return &tweet
 }
 
-func (s *SimpleTweetTable) UpdateLatestTweetInDynamo(t *SimpleTweetDTO) {
+// UpdateTweet is a DynamoOperator that allows us to Update the latest tweet
+type UpdateTweet struct{}
+
+// Execute from UpdateTweet allows us to Update the latest tweet in DynamoDB
+func (UpdateTweet) Execute(s *DynamoDbInstance, t *SimpleTweetDTO) *SimpleTweetDTO {
 	av, err := dynamodbattribute.MarshalMap(t)
 
 	fmt.Printf("%+v\n", av)
@@ -71,10 +94,16 @@ func (s *SimpleTweetTable) UpdateLatestTweetInDynamo(t *SimpleTweetDTO) {
 		TableName: aws.String(os.Getenv("TABLE_NAME")),
 	}
 
-	_, err = s.session.PutItem(input)
+	_, err = s.Session.PutItem(input)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to Put Item, %v", err))
 	}
 
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
 	fmt.Println("Successfully added tweet " + t.ID)
+	return t
+
 }
